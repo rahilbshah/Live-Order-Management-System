67440e0ecec85f99d505f7b23a02da99a0ab51ad
rahilbshah <98266707+rahilbshah@users.noreply.github.com>
2024-08-23 11:32:04 +0530

    Fix the spelling

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -86,7 +86,7 @@ const Cart = () => {
           <span className="">${totalAmount?.toFixed(2)}</span>
         </div>
         <div className="flex justify-between">
-          <span className="">Service Cost</span>
+          <span className="">Service Costs</span>
           <span className="">$0.00</span>
         </div>
         <div className="flex justify-between">
